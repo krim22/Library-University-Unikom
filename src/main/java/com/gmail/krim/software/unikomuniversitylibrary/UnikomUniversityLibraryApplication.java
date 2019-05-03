74ffda0d98ce2a76d4cae74bbc878f2ac5f85eea
krim22 <baghdadkrim@gmail.com>
2019-05-03 13:15:52 +0200

    update project

---
+++ src/main/java/com/gmail/krim/software/unikomuniversitylibrary/UnikomUniversityLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.krim.software.unikomuniversitylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnikomUniversityLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnikomUniversityLibraryApplication.class, args);
+	}
+
+}
